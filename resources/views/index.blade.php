34ff7bd461a01d7f1fe36450c5ea1af2873f8e8b
Smirnova <smirnova270694@gmail.com>
2020-01-05 00:56:36 +0200

    fixed style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="d-flex justify-content-center position-relative">
         <i class="fa fa-paperclip fa-5x paperclip" aria-hidden="true"></i>
-        <div class="card mt-5 sticker" style="width: 23rem; height: 23rem;">
+        <div class="card mt-5 sticker" style="width: 30rem; height: 30rem;">
             <div class="card-body text-center sticker-content">
                 <h5 class=" index-text">Welcome to Quizzes</h5>
                 <a class="select-category custom-link" href="{{route('categories.index')}}">select category</a>
